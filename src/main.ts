eebc6d6c8166c672e543b790a368f8a774be037b
IDONTSUDO <idontsudo@gmail.com>
2020-09-15 22:06:55 +0300

    initial

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+const { Client } = require('pg')
+const client = new Client({
+    user: 'USER',
+    host: 'localhost',
+    database: 'dbname',
+    password: 'myPassword',
+    port: 5432
+})
+;(async () => {
+  await client.connect()
+  const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+  console.log(res.rows[0].message) // Hello world!
+  await client.end()
+})()
