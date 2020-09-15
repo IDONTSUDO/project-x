b5aedecadbc05eec546f9cabd0f651fbca8c08d5
IDONTSUDO <idontsudo@gmail.com>
2020-09-15 22:51:34 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ const client = new Client({
 ;(async () => {
   await client.connect()
   const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-  console.log(res.rows[0].message) // Hello world!
+  console.log(res.rows[0].message) 
   await client.end()
 })()
